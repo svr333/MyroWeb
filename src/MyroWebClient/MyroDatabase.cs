14db2c8531f53c86c441afcec686c35b574fc1fd
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-01-21 20:10:57 +0100

    Implement basic json db

--- src/MyroWebClient/MyroDatabase.cs
+++ src/MyroWebClient/MyroDatabase.cs
@@ -1,19 +1,49 @@
 ﻿using MyroWebClient.Entities;
+using MyroWebClient.Exceptions;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MyroWebClient
 {
     public class MyroDatabase
     {
+        private MyroDataService _myro;
+
+        public MyroDatabase(MyroDataService myro)
+        {
+            _myro = myro;
+            LoadUsersInMemory();
+        }
+
         private string dbLocation = "Resources/users.json";
+        private ConcurrentDictionary<string, User> _users = new ConcurrentDictionary<string, User>();
+
+        private void LoadUsersInMemory()
+        {
+            if (File.Exists(dbLocation)) File.WriteAllText(dbLocation, "");
+
+            var json = File.ReadAllText(dbLocation);
+            _users = JsonConvert.DeserializeObject<ConcurrentDictionary<string, User>>(json);
+        }
 
         public User GetUserByKey(string key)
         {
-            return null;
+            _users.TryGetValue(key, out User user);
+
+            return user;
         }
 
         public void StoreNewUser(string key, User user)
         {
+            // VALIDATE IF USER IS CORRECT, IF NOT -> THROW
+            if (_myro.GetAllMyroData(user) == null) throw new WrongLoginException();
 
+            _users.AddOrUpdate(key, user, (oldKey, oldUser) => user);
+
+            var json = JsonConvert.SerializeObject(_users);
+            File.WriteAllText(dbLocation, json);
         }
     }
 }
