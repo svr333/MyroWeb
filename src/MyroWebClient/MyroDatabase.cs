d9d9afea2df2b9bd696a256d28459f6fbb57d7ec
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-01-24 18:57:47 +0100

    Fix rather big mistakes

--- src/MyroWebClient/MyroDatabase.cs
+++ src/MyroWebClient/MyroDatabase.cs
@@ -22,8 +22,8 @@ namespace MyroWebClient
         private void LoadUsersInMemory()
         {
             Directory.CreateDirectory("Resources");
-            if (File.Exists(dbLocation)) File.WriteAllText(dbLocation, "");
-            StoreNewUser("2", new User { UserName = "USERNAME", Password = "PASSWORD", SchoolAbreviation = "SCHOOLABREVIATION" });
+            if (!File.Exists(dbLocation)) File.WriteAllText(dbLocation, "");
+            
 
             var json = File.ReadAllText(dbLocation);
             _users = JsonConvert.DeserializeObject<ConcurrentDictionary<string, User>>(json);
