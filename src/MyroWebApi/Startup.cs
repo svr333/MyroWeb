0e97299e418df8ca68809bcf4e9164b7562525ed
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-02-09 13:20:01 +0100

    Clean up startup method

--- src/MyroWebApi/Startup.cs
+++ src/MyroWebApi/Startup.cs
@@ -26,14 +26,7 @@ namespace MyroWebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
